2ad2bb83e08a86f91f1b1041d398e4e67696f4f6
Srikar <msrikar328@gmail.com>
2025-07-22 19:29:05 +0530

    Add REALTOR to UserRole enum

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "🔧 Running Prisma generate..."
+npx prisma generate
+
+echo "📤 Pushing Prisma DB schema..."
+npx prisma db push --schema=backend/prisma/schema.prisma
+
+echo "🏗 Building NestJS..."
+npm run build:nest
