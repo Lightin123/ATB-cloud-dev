38748d2e75562cfd171a57100e09c3e4ee9407d5
Srikar <msrikar328@gmail.com>
2025-07-22 10:16:52 +0530

    Fix TS config and imports, remove ESM module type

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from 'C:\\ATB-local-testing-dev\\frontend\\src\\auth\\auth.controller.ts';
-import { AuthService } from 'C:\\ATB-local-testing-dev\\frontend\\src\\auth\\auth.service.ts';
+import { AuthController } from 'C:\\ATB-local-testing-dev\\backend\\src\\auth\\auth.controller';
+import { AuthService } from 'C:\\ATB-local-testing-dev\\backend\\src\\auth\\auth.service';
 
 @Module({
   controllers: [AuthController],
